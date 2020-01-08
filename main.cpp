4b9ffa8897f5a9f6b8115377544afbd955b131da
sky-0x00 <sky-0x00@mail.ru>
2020-01-08 15:58:36 +0300

    1) module 'trace' 2) buil-results + gitignore

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 // tracer.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include "trace.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int wmain(
+) {
+    
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
